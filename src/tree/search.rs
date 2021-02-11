82051421b2a141dc72e32ee20dc2462a888a5d1a
henrylee2cn <henrylee2cn@gmail.com>
2021-02-11 16:22:53 +0800

    fix(tree):  search:: catalan i32 -> u64
    Change-Id: I3ea7799254c1be091976544a31defc7d02909929

--- src/tree/search.rs
+++ src/tree/search.rs
@@ -1,10 +1,10 @@
 // catalan calculate how many binary search trees can be formed by node_num nodes
-pub fn catalan(node_num: i32) -> i32 {
+pub fn catalan(node_num: u64) -> u64 {
     let mut c: f64 = 1.0;
     for i in 0..node_num {
         c = c * 2.0 * (2.0 * i as f64 + 1.0) / (i as f64 + 2.0);
     }
-    c as i32
+    c as u64
 }
 
 #[cfg(test)]
@@ -16,6 +16,6 @@ mod tests {
         assert_eq!(1, catalan(1));
         assert_eq!(2, catalan(2));
         assert_eq!(5, catalan(3));
-        assert_eq!(2147483647, catalan(20))
+        assert_eq!(6564120420, catalan(20))
     }
 }
